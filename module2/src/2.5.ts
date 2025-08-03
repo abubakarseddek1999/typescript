5fb08862faead74df1b3718fa62531eb64d01c5e
Abu bakar <abubakarseddek199@gmail.com>
2025-08-03 22:39:51 +0600

    function wit generics

--- module2/src/2.5.ts
+++ module2/src/2.5.ts
@@ -3,7 +3,15 @@
 
 // function wit generics
 
+const createArray = (param: string): string[] => {
+    return [param];
+}
+const createArrayWithGenerics = <T>(param: T): T[] => {
+    return [param];
+}
 
+const res1 = createArray("Hello");
+const res2 = createArrayWithGenerics<boolean>(true);
 
 
 
